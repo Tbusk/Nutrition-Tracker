0de266e164115828a83f9b10bb58a2b6fcf29720
Trevor Busk <buskt@ferris.edu>
2023-10-22 03:45:21 -0400

    Moving registration controller to web package

--- Spring Web Project/src/main/java/com/SENG315/SpringJPA/web/RegistrationController.java
+++ Spring Web Project/src/main/java/com/SENG315/SpringJPA/web/RegistrationController.java
@@ -1,4 +1,4 @@
-package com.SENG315.SpringJPA.Security;
+package com.SENG315.SpringJPA.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
